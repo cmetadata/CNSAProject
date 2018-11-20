71e1803b4fb195c5f1900b95264ceddb569a7ef9
Erik <erik.henry@dcmail.ca>
2018-11-20 17:25:29 -0500

    contact page update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -14,14 +14,14 @@
   <h1 class="content-header grey-back">Contact Information</h1>
 
   <div class="object-div grey-back">
-    <table width="35%">
+    <table width="42%">
     	<tr>
-    		<td width="10%"><a href=#><b>Phone Number:</b></a></td>
-    		<td width="10%"><a href=#>(999) 123-4567</a></td>
+    		<td width="10%"><b>Phone Number:</b></td>
+    		<td width="10%">(999) 123-4567</td>
     	</tr>
     	<tr>
-    		<td width="10%"><a href=#><b>Email:</b></a></td>
-    		<td width="10%"><a href=#>example@placeholder.com</a></td>
+    		<td width="10%"><b>Email:</b></td>
+    		<td width="10%">example@placeholder.com</td>
     	</tr>
     </table>
   </div>
@@ -29,6 +29,8 @@
   <br/>
 
   <a href="https://twitter.com/intent/tweet?screen_name=CNSA&ref_src=twsrc%5Etfw" class="twitter-mention-button" data-show-count="false">Tweet to @CNSA</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+  <br/>	
 
   <div class="fb-share-button" data-href="https://www.facebook.com/CNSA/?ref=br_rs" data-layout="button" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.facebook.com%2FCNSA%2F%3Fref%3Dbr_rs&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">CNSA</a></div>
 
