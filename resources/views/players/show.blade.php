65cea798343ae45bf5142903b9d3b80e22f9ce61
Jessica Querido <jessica.querido@dcmail.ca>
2018-11-20 12:21:40 -0500

    committing to pull

---
+++ resources/views/players/show.blade.php
@@ -0,0 +1,12 @@
+@extends ('layout.header')
+
+@section ('content')
+
+  <h1 class="content-header grey-back">[Player Name]</h1>
+
+  <div class="object-div grey-back">
+    <a href=#>[Team Name]</a>
+    <a href=#>[School Name]</a>
+  </div>
+
+@endsection
