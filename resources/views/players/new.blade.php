948adb1ddc570477d449eb3d86e85f16dffea3c3
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-11-19 23:11:33 -0500

    removed route register

--- resources/views/players/new.blade.php
+++ resources/views/players/new.blade.php
@@ -9,7 +9,7 @@
                 <div class="card-header">{{ __('Register Player') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" >
                         @csrf
 
                         <div class="form-group row">
