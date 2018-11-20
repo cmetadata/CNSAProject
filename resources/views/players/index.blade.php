df76b5f0498f1335e3008ee38291ad57c70b4025
Jessica Querido <jessica.querido@dcmail.ca>
2018-11-20 16:48:26 -0500

    committing to pull

--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -3,33 +3,29 @@
 @section ('content')
 
 <div>
-    <div>
-        <div class='btn-toolbar pull-right'>
-            <div class='btn-group'>
-                <button type='button' class='btn btn-default new-button grey-back'>Add New Player</button>
-            </div>
+    <div class='btn-toolbar pull-right'>
+        <div class='btn-group'>
+            <button name="create-button" type='button' class='btn btn-default new-button grey-back'>Add New Player</button>
         </div>
-         <h1 class="content-header grey-back">All Players</h1>
     </div>
+     <h1 class="content-header grey-back">All Players</h1>
 </div>
-
 <hr/>
-<!--
-  <div>
-  <h1 class="content-header grey-back">All Players</h1>
-  <div class='btn-toolbar pull-right'>
-  <button type="submit" class="btn btn-default new-button" href="https://cnsa-project.herokuapp.com/players/create">Add New Player</button>
-  </div>
-  </div> -->
-
-  <div class="object-div grey-back">
-    <table width="100%">
-    	<tr>
-    		<td width="40%"><a href=#>Name Here</a></td>
-    		<td width="50%"><a href=#>Team Here</a></td>
-    		<td width="10%">Position Here</td>
-    	</tr>
-    </table>
-  </div>
+<div>
+    <div class='btn-toolbar pull-right'>
+        <div class='btn-group'>
+            <button name="edit-button" type='button' class='btn btn-default edit-button grey-back'>Edit Player</button>
+        </div>
+    </div>
+	  <div class="object-div grey-back">
+	    <table width="100%">
+	    	<tr>
+	    		<td width="40%"><a href=#>Name Here</a></td>
+	    		<td width="50%"><a href=#>Team Here</a></td>
+	    		<td width="10%">Position</td>
+	    	</tr>
+	    </table>
+	  </div>
+</div>
 
 @endsection
