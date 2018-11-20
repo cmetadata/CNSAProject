96b129caecd619bd0ed66aecc643b5aea4e8b0a9
whatisc <jah_10@live.com>
2018-11-20 14:54:19 -0500

    modified new.blade.php as per specs

--- resources/views/players/new.blade.php
+++ resources/views/players/new.blade.php
@@ -9,13 +9,23 @@
   <div class="object-div grey-back">
 
         <div class="form-group">
-            <label for="email">Email address:</label>
-            <input type="email" class="form-control" id="email">
+            <label for="playerFirst">First Name:</label>
+            <input type="text" class="form-control" id="playerFirst">
         </div>
 
         <div class="form-group">
-            <label for="pwd">Password:</label>
-            <input type="password" class="form-control" id="pwd">
+            <label for="playerLast">Last Name:</label>
+            <input type="text" class="form-control" id="playerLast">
+        </div>
+
+        <div class="form-group">
+            <label for="playerPosition">Position:</label>
+            <input type="text" class="form-control" id="playerPosition">
+        </div>
+
+        <div class="form-group">
+            <label for="playerSchool">School:</label>
+            <input type="text" class="form-control" id="playerSchool">
         </div>
 
         <button type="submit" class="btn btn-default">Submit</button>
