ca6094b6ecf6578e8d3c08ab0a0cea6ac019f5f6
Mathew Ricketts <mathew.ricketts@dcmail.ca>
2018-11-21 15:48:29 -0500

    Working factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,21 +28,3 @@ $factory->define(App\Player::class, function (Faker $faker) {
 		'assists'		=> $faker->number,
 		'saves' 		=> $faker->number
 });
-
-$factory->define(App\Player::class, function (Faker\Generator $faker) {	
-	//Creating random data for the database Player table
-	return [
-		'firstName' 	=> $faker->first_name,
-		'lastName' 		=> $faker->last_name,
-		'schoolId' 		=> $faker->number,
-		'yearEntered' 	=> $faker->date,
-		'position' 		=> str_random(10),
-		'teamId' 		=> $faker->number,
-		'redCards' 		=> $faker->number,
-		'yellowCards' 	=> $faker->number,
-		'goals' 		=> $faker->number,
-		'playerRating' 	=> $faker->number,
-		'assists'		=> $faker->number,
-		'saves' 		=> $faker->number
-	];
-});
