6a7af707bb59c16244d4a60ca34c9da731fd8902
whatisc <jah_10@live.com>
2018-11-21 09:49:03 -0500

    fixed playerPosition var

--- app/Http/Controllers/PlayersController.php
+++ app/Http/Controllers/PlayersController.php
@@ -66,7 +66,7 @@ class PlayersController extends Controller
 
         $player->playerRating = $request->playerRating;
 
-        $player->position = $request->position;
+        $player->playerPosition = $request->playerPosition;
 
         $player->schoolId = $request->schoolId;
 
