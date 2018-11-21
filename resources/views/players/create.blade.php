e2a54ee7252f1c07e28859c5c86300f43d6d81ca
whatisc <jah_10@live.com>
2018-11-21 11:39:45 -0500

    basic html validation

--- resources/views/players/create.blade.php
+++ resources/views/players/create.blade.php
@@ -12,37 +12,37 @@
 
             <div class="form-group">
                 <label for="firstName">First Name:</label>
-                <input type="text" class="form-control" id="firstName" name="firstName">
+                <input type="text" class="form-control" id="firstName" name="firstName" required>
             </div>
 
             <div class="form-group">
                 <label for="lastName">Last Name:</label>
-                <input type="text" class="form-control" id="lastName" name="lastName">
+                <input type="text" class="form-control" id="lastName" name="lastName" required>
             </div>
 
             <div class="form-group">
                 <label for="yearEntered">Year Entered:</label>
-                <input type="text" class="form-control" id="yearEntered" name="yearEntered">
+                <input type="text" class="form-control" id="yearEntered" name="yearEntered" required>
             </div>            
 
             <div class="form-group">
                 <label for="playerRating">Player Rating:</label>
-                <input type="text" class="form-control" id="playerRating" name="playerRating">
+                <input type="text" class="form-control" id="playerRating" name="playerRating" required>
             </div>            
 
             <div class="form-group">
                 <label for="position">Position:</label>
-                <input type="text" class="form-control" id="position" name="position">
+                <input type="text" class="form-control" id="position" name="position" required>
             </div>
 
             <div class="form-group">
                 <label for="schoolId">School ID:</label>
-                <input type="text" class="form-control" id="schoolId" name="schoolId">
+                <input type="text" class="form-control" id="schoolId" name="schoolId" required>
             </div>
 
             <div class="form-group">
                 <label for="teamId">Team ID:</label>
-                <input type="text" class="form-control" id="teamId" name="teamId">
+                <input type="text" class="form-control" id="teamId" name="teamId" required>
             </div>
             
 
