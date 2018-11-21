2b90eee0ded3ebcd73e8eff210bfb2e5a9403c2b
Mathew Ricketts <mathew.ricketts@dcmail.ca>
2018-11-21 15:27:32 -0500

    Please work

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,7 +10,7 @@ use Faker\Generator as Faker;
 | This directory should contain each of the model factory definitions for
 | your application. Factories provide a convenient way to generate new
 | model instances for testing / seeding your application's database.
-|
+|Player
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
