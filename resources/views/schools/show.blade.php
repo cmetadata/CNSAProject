7a8ddba458a2b37cb8f7611aace9f1c7d629e6a8
Jessica Querido <jessica.querido@dcmail.ca>
2018-11-20 20:59:28 -0500

    Added a show for all the relevant objects. Sorta.

---
+++ resources/views/schools/show.blade.php
@@ -0,0 +1,15 @@
+@extends ('layout.header')
+
+@section ('content')
+
+  <h1 class="content-header grey-back">[School Name]</h1>
+
+    <div class=info-page>
+    Rating: [School Rating]<br/>
+    <br/>
+
+    [INCLUDE HERE A LIST OF ALL THE SCHOOL'S TEAMS]
+
+  </div>
+
+@endsection
