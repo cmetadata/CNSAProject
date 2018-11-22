ac13f9722775fe17c41d8b4b96ce61cd2734b094
whatisc <jah_10@live.com>
2018-11-22 14:05:03 -0500

    removed players var

--- resources/views/teams/team.blade.php
+++ resources/views/teams/team.blade.php
@@ -12,14 +12,6 @@
             <td width="40%"><a href="/teams/{{$team->teamId}}">{{$team->teamName}} </a></td>
 
 
-            <!-- foreach loop which displays player
-            
-                @foreach ($players as $player)
-                    @include ('players.player')
-                    <br/>
-                @endforeach     
-            
-             -->
         </tr>
       </table>
     </div>
