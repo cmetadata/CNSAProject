ae1f037e6b648c2e854b81e4b64850e8fd2b3536
Jessica Querido <jessica.querido@dcmail.ca>
2018-11-22 12:38:44 -0500

    fixing schools controller

--- resources/views/schools/school.blade.php
+++ resources/views/schools/school.blade.php
@@ -9,7 +9,7 @@
     <div class="object-div grey-back">
       <table width="100%">
         <tr>
-          <td width="50%"><a href=#>School Name</a></td>
+          <td width="50%"><a href="/schools/{{$school->schoolId}}">School Name</a></td>
           <td width="25%">High School Athletes?</td>
           <td width="25%">School Ranking: 0</td>
         </tr>
