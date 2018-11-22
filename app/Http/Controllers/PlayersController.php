3d43e31b70c85d2f34076b2996b26c5dcb9bbc84
Jessica Querido <jessica.querido@dcmail.ca>
2018-11-22 10:43:15 -0500

    meh

--- app/Http/Controllers/PlayersController.php
+++ app/Http/Controllers/PlayersController.php
@@ -105,7 +105,7 @@ class PlayersController extends Controller
     {
         $player = Player::find($request->playerId);
         $team = Team::find($request->teamId);
-        return view('players.show', compact('player'));
+        return view('players.show', compact(['player', 'team']));
     }
 
 
