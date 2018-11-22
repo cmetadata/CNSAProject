184abe3f20e8b1c1c51e381afb1a207118e90106
Mathew Ricketts <mathew.ricketts@dcmail.ca>
2018-11-22 14:04:06 -0500

    Migrations

--- database/migrations/2018_11_12_184628_create_game_stats_table.php
+++ database/migrations/2018_11_12_184628_create_game_stats_table.php
@@ -16,7 +16,7 @@ class CreateGameStatsTable extends Migration
         Schema::create('game_stats', function (Blueprint $table) {
             $table->increments('statId');
             $table->integer('teamId');
-            $table->date('teamScore');
+            $table->integer('teamScore');
             $table->integer('gameId');
             $table->timestamps();
 
