581ed94b6987bec7f87d593895446e00fdee7006
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-11-22 22:00:00 -0500

    needs an index

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -2,7 +2,7 @@
 
 @section ('content')
 
-  <h1 class="content-header grey-back">{{$team->teamName}}</h1>
+  <h1 class="content-header grey-back">{{$team[0]->teamName}}</h1>
 
     <div class=info-page>
     <!-- players -->
