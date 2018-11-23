9369c2de711e5ab70cf6829295b7afc515ee8f62
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-11-22 21:46:25 -0500

    team ranking

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -5,7 +5,7 @@
   <h1 class="content-header grey-back">{{$teams->teamName}}</h1>
 
     <div class=info-page>
-      <a href=/schools/{{ $school->schoolId }}> {{ $school->schoolName }} </a>
+    {{ $teams->teamRanking }} 
     <br/></br/>
   </div>
 
