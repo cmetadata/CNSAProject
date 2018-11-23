8e18cd851b71510ad4226689f2ec8b56eb984927
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-11-22 22:25:17 -0500

    is set on link

--- resources/views/players/player.blade.php
+++ resources/views/players/player.blade.php
@@ -10,12 +10,14 @@
 	    <table width="100%">
 	    	<tr>
 	    		<td width="25%"><a href="/players/{{$player->playerId}}">{{$player->firstName}} {{$player->lastName}}</a></td>
-	    		<td width="25%"><a href="/teams/{{$team[$loop->index]->teamId}}">
+	    		<td width="25%"><a href="/teams/@isset($team[$loop->index])
+						{{ $team[$loop->index]->teamName }}
+					@endisset">
 				
 					@isset($team[$loop->index])
 						{{ $team[$loop->index]->teamName }}
 					@endisset
-					
+
 				</a></td>
 				<td width="25%"><a href="/schools/{{$school[$loop->index]->schoolId}}">{{$school[$loop->index]->schoolName}}</a></td>
 	    		<td width="25%">{{$player->position}}</td>
