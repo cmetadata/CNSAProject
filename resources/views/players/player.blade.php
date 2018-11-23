7c2b980b7a148f4aad9c2e508dc874506a00f8d1
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-11-22 22:23:50 -0500

    if isset

--- resources/views/players/player.blade.php
+++ resources/views/players/player.blade.php
@@ -10,7 +10,13 @@
 	    <table width="100%">
 	    	<tr>
 	    		<td width="25%"><a href="/players/{{$player->playerId}}">{{$player->firstName}} {{$player->lastName}}</a></td>
-	    		<td width="25%"><a href="/teams/{{$team[$loop->index]->teamId}}">{{$team[$loop->index]->teamName}}</a></td>
+	    		<td width="25%"><a href="/teams/{{$team[$loop->index]->teamId}}">
+				
+					@isset($team[$loop->index])
+						{{ $team[$loop->index]->teamName }}
+					@endisset
+					
+				</a></td>
 				<td width="25%"><a href="/schools/{{$school[$loop->index]->schoolId}}">{{$school[$loop->index]->schoolName}}</a></td>
 	    		<td width="25%">{{$player->position}}</td>
 	    	</tr>
