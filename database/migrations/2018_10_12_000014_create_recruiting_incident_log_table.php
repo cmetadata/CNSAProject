16d2baa6af95467e80416ce3a5f71552906ec7c1
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-11-25 16:07:31 -0500

    added logtable to migrations

--- database/migrations/2018_10_12_000014_create_recruiting_incident_log_table.php
+++ database/migrations/2018_10_12_000014_create_recruiting_incident_log_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRecruitingIncidentTable extends Migration
+class CreateRecruitingIncidentLogTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_10_12_000015_create_scholarships_log_table.php
+++ database/migrations/2018_10_12_000015_create_scholarships_log_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateScholarshipsTable extends Migration
+class CreateScholarshipsLogTable extends Migration
 {
     /**
      * Run the migrations.
