c996e49378fc88b056ba4e632dd873f6dd09191e
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-11-24 20:52:09 -0500

    $table defined as persons ... may need to update later

--- app/Person.php
+++ app/Person.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
+    protected $table = 'persons';
 	//Setting the primary key to be named something other than id
     protected $primaryKey = 'personId';
 
