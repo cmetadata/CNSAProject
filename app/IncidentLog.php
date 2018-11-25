6cd639a788694d5ec32937c9e20f2e9323ef18b5
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-11-25 16:34:10 -0500

    removed school

--- app/IncidentLog.php
+++ app/IncidentLog.php
@@ -8,10 +8,4 @@ class IncidentLog extends Model
 {
 	protected $table = 'recruiting_incident_logs';
 	protected $primaryKey = 'logId';
-
-	//an incident belongs to one schoolId
-	public function school()
-    {
-    	return $this->belongsTo(School::class);
-    }
 }
