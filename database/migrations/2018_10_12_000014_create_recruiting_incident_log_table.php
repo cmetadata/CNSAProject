896c44e14cf50d9a532dcc1942d32f3256cc79d6
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-11-25 16:10:07 -0500

    on recruiting_incident - migration

--- database/migrations/2018_10_12_000014_create_recruiting_incident_log_table.php
+++ database/migrations/2018_10_12_000014_create_recruiting_incident_log_table.php
@@ -29,7 +29,7 @@ class CreateRecruitingIncidentLogTable extends Migration
 
         Schema::table('recruiting_incident_logs', function($table) {
             //Setting up the relationships
-            $table->foreign('incidentId')->references('incidentId')->on('incidents')->onDelete('cascade');
+            $table->foreign('incidentId')->references('incidentId')->on('recruiting_incident')->onDelete('cascade');
             $table->foreign('schoolId')->references('schoolId')->on('schools')->onDelete('cascade');
             $table->foreign('playerId')->references('playerId')->on('players')->onDelete('cascade');
         });
