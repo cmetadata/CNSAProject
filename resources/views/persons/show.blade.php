63fd8531b334eb358573ed29a2eb60f672f324ff
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-11-24 20:56:35 -0500

    updated for loop

--- resources/views/persons/show.blade.php
+++ resources/views/persons/show.blade.php
@@ -7,6 +7,7 @@
     <!--Include a list of all teams belonging to the school-->
     Positions for this person:<br/>
     @foreach ($positions as $position)
+      {{$person->personFirstName}} {{$person->personLastName}}
       @include ('positions.position')
       <br/>
     @endforeach
