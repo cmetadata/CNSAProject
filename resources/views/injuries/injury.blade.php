603c389e4163279ea26ead0c856d2385acd51fef
whatisc <jah_10@live.com>
2018-11-27 15:19:11 -0500

    fixed word

--- resources/views/injuries/injury.blade.php
+++ resources/views/injuries/injury.blade.php
@@ -3,7 +3,7 @@
 <div>
     <div class='btn-toolbar pull-right'>
         <div class='btn-group'>
-            <button name="edit-button" type='button' class='btn btn-default edit-button grey-back'>Edit Incident</button>
+            <button name="edit-button" type='button' class='btn btn-default edit-button grey-back'>Edit Injury</button>
         </div>
     </div>
     <div class="object-div grey-back">
