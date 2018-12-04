69faa652734670dc90271f8933a41341f284ad61
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-12-03 20:08:42 -0500

    updated names

--- database/migrations/2018_10_12_000009_create_public_recruiting_incident_logs_table.php
+++ database/migrations/2018_10_12_000009_create_public_recruiting_incident_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePublicRecruitingIncidentTable extends Migration
+class CreatePublicRecruitingIncidentLogsTable extends Migration
 {
     /**
      * Run the migrations.
