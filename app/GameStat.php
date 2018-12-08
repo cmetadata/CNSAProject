f931346900c2542e08f8fc1dafc94bf704a99683
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-12-08 16:02:31 -0500

    from game stat too

--- app/GameStat.php
+++ app/GameStat.php
@@ -7,9 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class GameStat extends Model
 {
     //Setting the primary key to be named something other than id
-    protected $table = 'game_stats';
-
-    protected $primaryKey = 'statId';
 
     public function stadium()
     {
