6e0051b066fc6bf9cf5adaadda5bf66cb36d6b39
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-12-08 15:03:21 -0500

    die and dump this

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -31,6 +31,12 @@ class GamesController extends Controller
             // this returns a collection into the gameStats array
             $gameStats[$i] = GameStat::all()->where('gameId', $game->gameId);
 
+
+            dd($gameStats);
+        
+            dd(gameStats()->all());
+
+
             // find the team based on teamId on gameStats array element
             $teams1[$i] = Team::find($gameStats[$i]->get($i)->teamId);
             
