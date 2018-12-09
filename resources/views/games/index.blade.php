71b482b47b3d9b912ae62486902ece68799692fc
Jonathan Hermans <jonathan.hermans@dcmail.ca>
2018-12-08 20:00:05 -0500

    proper modulus

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -16,11 +16,7 @@
 
     {{ $index = 0 }}
 
-    @if ($loop->index === 1)
-        {{ $index = 2 }}
-    @endif
-
-    @if ($loop->index % 2 === 1)
+    @if (($loop->index % 2) === 1)
         {{ $index = $loop->index + 1 }} 
     @endif
     
